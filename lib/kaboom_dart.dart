fecafab82b5861ee86d224ec14b450602cee2fb3
Tanveer Najib <tanveernajib@gmail.com>
2022-04-10 12:06:30 +0100

    export models and bump ver

--- lib/kaboom_dart.dart
+++ lib/kaboom_dart.dart
@@ -6,8 +6,10 @@ library kaboom_dart;
 export 'src/kaboom_dart_base.dart';
 export 'src/options.dart';
 export 'src/models/comics_models.dart';
+export 'src/models/accounts_models.dart';
 export 'src/models/cartoons_models.dart';
 export 'src/models/auth_models.dart';
 export 'src/models/results.dart';
+export 'src/models/social_models.dart';
 
 // Export any libraries intended for clients of this package.
